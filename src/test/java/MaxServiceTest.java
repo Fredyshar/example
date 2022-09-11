0dd5e814858d9cb9636511707f551b58c24dea59
Fedor Sharov <1710193@mail.ru>
2022-09-11 20:56:15 +0300

    new 100%

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,5 +1,6 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
 
 public class MaxServiceTest {
     @Test
@@ -10,6 +11,18 @@ public class MaxServiceTest {
         int b = 3;
 
         int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldFindMax2() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
